dc04588a82619ca1b377689ea63829f8fbef8e58
David Piepgrass <qwertie256@gmail.com>
2018-08-09 17:50:59 -0600

    Improve 4-CalendarEntry

--- react-examples/4-CalendarEntry/app.tsx
+++ react-examples/4-CalendarEntry/app.tsx
@@ -1,120 +1,121 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import {parseTime, timeToStringUTC} from './time';
+import { timeToString, parseTime } from 'simplertime/dist/simplertime';
 
 interface TimeSelectorProps {
+  day: Date;
   time?: Date;
-  onTimeChange: (time?:Date) => void;
+  onChange: (time?: Date) => void;
+  style?: React.CSSProperties;
+  class?: string;
 }
 
-class TimeSelector extends React.Component<TimeSelectorProps,
-                                           {timeInput?:string}>
-{
-  state = { timeInput: undefined as (string|undefined) };
-  render() {
-    let timeString = "";
-    if (this.state.timeInput !== undefined)
-        timeString = this.state.timeInput;
-    else if (this.props.time !== undefined)
-        timeString = timeToStringUTC(this.props.time, false);
+function TimeSelector(p: TimeSelectorProps) {
+  const TimeFmt = {showSeconds:false, use24hourTime:true};
+  return (
+    <input type="time" style={p.style} className={p.class || "timeSelector"}
+           value={p.time ? timeToString(p.time, TimeFmt) : ""}
+           onChange={ e => p.onChange(parseTime(e.target.value, p.day)) }/>);
+}
 
-    return (<span>
-        <input type="text" list="times" style={ {width:75} }
-               value={timeString}
-               onChange={ e => this.setState({timeInput: e.target.value}) }
-               onBlur={ e => {
-                 this.setState({timeInput: undefined});
-                 this.props.onTimeChange(parseTime(e.target.value)); 
-               } }/>
-        <datalist id="times">
-          <option value="12:00am"/><option value="12:30am"/>
-          <option value="1:00am"/><option value="1:30am"/>
-          <option value="2:00am"/><option value="2:30am"/>
-          <option value="3:00am"/><option value="3:30am"/>
-          <option value="4:00am"/><option value="4:30am"/>
-          <option value="5:00am"/><option value="5:30am"/>
-          <option value="6:00am"/><option value="6:30am"/>
-          <option value="7:00am"/><option value="7:30am"/>
-          <option value="8:00am"/><option value="8:30am"/>
-          <option value="9:00am"/><option value="9:30am"/>
-          <option value="10:00am"/><option value="10:30am"/>
-          <option value="11:00am"/><option value="11:30am"/>
-          <option value="12:00pm"/><option value="12:30pm"/>
-          <option value="1:00pm"/><option value="1:30pm"/>
-          <option value="2:00pm"/><option value="2:30pm"/>
-          <option value="3:00pm"/><option value="3:30pm"/>
-          <option value="4:00pm"/><option value="4:30pm"/>
-          <option value="5:00pm"/><option value="5:30pm"/>
-          <option value="6:00pm"/><option value="6:30pm"/>
-          <option value="7:00pm"/><option value="7:30pm"/>
-          <option value="8:00pm"/><option value="8:30pm"/>
-          <option value="9:00pm"/><option value="9:30pm"/>
-          <option value="10:00pm"/><option value="10:30pm"/>
-          <option value="11:00pm"/><option value="11:30pm"/>
-        </datalist>
-      </span>);
-  }
+/** Add a certain number of minutes to a Date */
+function addMinutes(date: Date, minutes: number) {
+  return new Date(date.valueOf() + minutes * 60000);
+}
+/** Gets the difference between two Dates in minutes */
+function diffMinutes(high: Date, low: Date) {
+  return (high.valueOf() - low.valueOf()) / 60000;
+}
+
+function FormRow(p: { label: React.ReactNode, children: React.ReactNode }) {
+  return (
+    <p>
+      <label>
+        <span className="label">{p.label}</span>
+        {p.children}
+      </label>
+    </p>);
 }
 
 interface CalendarEntry {
   eventName: string;
+  location: string;
   allDay: boolean;
   startTime?: Date; // Allow undefined when no valid start time is given
   durationMinutes: number;
   alarmOn: boolean;
   alarmMinutes: number;
+  color: string;
 }
 
 class CalendarEntryEditor extends React.Component<{}, CalendarEntry> {
   state = {
-    eventName: 'Daily run',
+    eventName: "Daily run",
+    location: "",
     allDay: false, 
     startTime: undefined,
     durationMinutes: 60,
     alarmOn: false,
     alarmMinutes: 5,
+    color: "#DDFF00"
   }
+  
   render() {
+    let minuteStyle = {width: "3rem"};
     var timeRangeElements: JSX.Element[] = [];
     if (!this.state.allDay) {
       let startTime = this.state.startTime;
       let endTime = startTime===undefined ? undefined :
           addMinutes(startTime, this.state.durationMinutes);
-      console.log(endTime?timeToStringUTC(endTime,false):'undef');
       timeRangeElements = [
-        <p>Start time:&nbsp;
-          <TimeSelector time={this.state.startTime}
-                onTimeChange={ time => this.setState({startTime: time}) }/></p>,
-        <p>End time:&nbsp;
-          <TimeSelector time={endTime}
-                onTimeChange={ time => this.setEndTime(time) }/>&nbsp;
-          (<input type="number" style={ {width:50} } step={5} min={0} max={24*60}
+        <FormRow label="Start time:">
+          <TimeSelector time={this.state.startTime} day={new Date()} 
+            onChange={ time => this.setState({startTime: time}) }/>
+        </FormRow>,
+        <FormRow label="End time:">
+          <TimeSelector time={endTime} day={new Date()} 
+            onChange={ time => this.setEndTime(time) }/>&nbsp;
+          (<input type="number" className="minuteCount" step={5} min={0} max={24*60}
              onChange={e => this.setState({durationMinutes: 
                      e.target.valueAsNumber || this.state.durationMinutes})}
              value={this.state.durationMinutes}/> minutes).
-        </p>
-      ];
-    }
-    return (
-      <div style={ {width: 300} }>
-        <p><input type="text" style={ {width:280} } 
-                  onChange={e=>this.setState({eventName: e.target.value})}
-                  value={this.state.eventName}/></p>
-        <p style={ {float: 'right', margin: '0 40px 0 0'} }>
-          <input type="checkbox" checked={this.state.allDay}
-            onChange={e => this.setState({allDay: e.target.checked})}/>All day
-        </p>
-        {timeRangeElements}
-        <p style={ {clear: 'both'} }>
-          <input type="checkbox" checked={this.state.alarmOn}
-            onChange={e => this.setState({alarmOn: e.target.checked})}/>Alarm&nbsp;
-          <input type="number" style={ {width:40} } min={0} max={720} 
+        </FormRow>,
+        <p style={ {clear: 'both' } }>
+          <label className="label">
+            <input type="checkbox" checked={this.state.alarmOn}
+              onChange={e => this.setState({alarmOn: e.target.checked})}/>Alarm&nbsp;
+          </label>
+          <input type="number" className="minuteCount" min={0} max={720} 
             onChange={e => this.setState({alarmMinutes: 
                 e.target.valueAsNumber || this.state.alarmMinutes, alarmOn: true})}
               value={this.state.alarmMinutes}/> minutes before
         </p>
-      </div>);
+      ];
+    }
+    return (
+      <form className="calendarEntryForm">
+        <p>
+          <input type="text" style={ {width:"24.2em"} } 
+                 onChange={e=>this.setState({eventName: e.target.value})}
+                 value={this.state.eventName}/>
+        </p>
+        <FormRow label="Location:">
+            <input type="text" style={ {width:"17em"} } 
+                   onChange={e=>this.setState({location: e.target.value})}
+                   value={this.state.location}/>
+        </FormRow>
+        <label style={ {display: "block", float: 'right', margin: '0 4em 0 0'} }>
+          <input type="checkbox" checked={this.state.allDay}
+            onChange={e => this.setState({allDay: e.target.checked})}/>All day
+        </label>
+        {timeRangeElements}
+        <FormRow label="Color:">
+          <input type="color" className="width: 7em" value={this.state.color}
+                 onChange={ e => this.setState({ color: e.target.value }) }/>
+        </FormRow>
+      </form>);
   }
+
   setEndTime(time?: Date) {
     if (this.state.startTime && time) {
       // ! means "assume this value is not null or undefined"
@@ -124,14 +125,6 @@ class CalendarEntryEditor extends React.Component<{}, CalendarEntry> {
       this.setState({durationMinutes: dif});
     }
   }
-}
-
-// Functions to add minutes to a Date or get the difference between two dates
-function addMinutes(date: Date, minutes: number) {
-  return new Date(date.valueOf() + minutes * 60000);
-}
-function diffMinutes(high: Date, low: Date) {
-  return (high.valueOf() - low.valueOf()) / 60000;
 }
 
 ReactDOM.render(<CalendarEntryEditor/>, document.getElementById("app"));
